fa14afb4f8f28429704f10971d0572df861691fd
Sudinach <sudin.ach16@gmail.com>
2026-05-13 01:34:22 +0000

    removed url={post.coverImage.url}

--- frontend/app/posts/[slug]/page.tsx
+++ frontend/app/posts/[slug]/page.tsx
@@ -47,7 +47,7 @@ export default async function PostPage({
           )}
         </div>
         <div className="mb-8 sm:mx-0 md:mb-16">
-          <CoverImage title={post.title} url={post.coverImage.url} />
+          <CoverImage title={post.title}/>
         </div>
         <div className="mx-auto max-w-2xl">
           <div className="mb-6 block md:hidden">
